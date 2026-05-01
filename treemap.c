809912d1124c129a73c6c46a4d70baaea2371599
Your Name <you@example.com>
2026-05-01 23:29:40 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -207,7 +207,7 @@ Pair * nextTreeMap(TreeMap * tree) {
     }
     else{
         TreeNode *aux = current->parent;
-        while(aux != NULL || aux->pair->key < current->pair->key){
+        while(aux != NULL && lower_than(aux->pair->key, current->pair->key) == 1){
             aux = aux->parent;
         }
         next = aux;
