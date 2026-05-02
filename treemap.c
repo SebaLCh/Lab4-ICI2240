ae33d46ca92ac9a194b8c26f3a130c39a9dc501b
Your Name <you@example.com>
2026-05-02 00:23:16 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -230,10 +230,11 @@ Pair * upperBound(TreeMap * tree, void* key) {
         TreeNode *aux = tree->root;
         Pair *ub_node = aux->pair;
         while(aux != NULL){
+            if(aux->pair->key > key && aux->pair->key < ub_node->key) ub_node = aux->pair;
+            
             if(tree->lower_than(key, aux->pair->key)) aux = aux->left;
             else if(tree->lower_than(aux->pair->key, key)) aux = aux->right;
-
-            if(aux->pair->key > key && aux->pair->key < ub_node->key) ub_node = aux->pair;
+            
         }
         parB = ub_node;
     }
